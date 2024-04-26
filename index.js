1dc4e69d8b1397f9a198be40bb99cec6260393c2
harshit <harshitshrivastava340@gmail.com>
2024-04-27 00:35:15 +0530

    axios fetchuser

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const redux  = require('redux');
+const reduxLogger = require('redux-logger');
 const createStore= redux.createStore
+const combineReducer =  redux.combineReducers
+const logger = reduxLogger.createLogger()
 
 // action of buying a cake
 
 const Buy_CAKE = 'Buy_CAKE'
+const Buy_ICECREAM = 'Buy_ICECREAM'
 function buycake () {
+    return {
+        type : Buy_CAKE,
+        info : "this is my first action"
+    }
+}
+function buyicecream () {
     return {
         type : Buy_CAKE,
         info : "this is my first action"
@@ -12,11 +22,14 @@ function buycake () {
 }
 
 //reducer
-const initial_state = {
+const initialcake = {
     num_of_cake : 10,
 }
+const initialicecream = {
+    numoficecream : 10,
+}
 
-const reducer = (state = initial_state, action ) => {
+const reducercake = (state = initialcake, action ) => {
   switch(action.type){
     case Buy_CAKE:return {
         ...state,
@@ -27,11 +40,28 @@ const reducer = (state = initial_state, action ) => {
   }
 
 }
+const reducericecream = (state = initialicecream, action ) => {
+  switch(action.type){
+    case Buy_CAKE:return {
+        ...state,
+        numoficecream: state.numoficecream-1
+    }
 
-const store = createStore(reducer)
+    default:return state 
+  }
+
+}
+//combine the multiple reducers 
+const rootreducer = combineReducer({
+  cake:reducercake,
+  icecream : reducericecream
+})
+const store = createStore(rootreducer,redux.applyMiddleware(logger))
 console.log('initial state',store.getState())
-const unsubscribe = store.subscribe(()=>console.log('updated store',store.getState()))
+const unsubscribe = store.subscribe(()=>{})
 store.dispatch(buycake())
 store.dispatch(buycake())
 store.dispatch(buycake())
+store.dispatch(buyicecream())
+store.dispatch(buyicecream())
 unsubscribe()
